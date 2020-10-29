8fdde6d1ad5d3c7752fee64eaba2a45ce0d8a208
Alex Siegman <asiegman@gmail.com>
2020-10-29 18:15:17 -0500

    Initial WASM Project; Beginnings of FileWatcher

---
+++ Web/wwwroot/main.js
@@ -0,0 +1,23 @@
+﻿
+let fileHandle;
+
+window.ChooseFile = async () => {
+    let fileHandlers = await window.showOpenFilePicker({ multiple: false });
+    if (fileHandlers.length > 0) {
+        fileHandle = fileHandlers[0];
+        return fileHandle['name'];
+    }
+}
+
+window.ReadFile = async () => {
+    const file = await fileHandle.getFile();
+    const contents = await file.text();
+    console.log("Reading file: " + fileHandle['name'] + " and got " + contents.length + " things");
+    return contents;
+}
+
+window.SaveFile = async () => {
+    const file = await fileHandle.createWritable();
+    await file.write(contents);
+    await file.close();
+}
